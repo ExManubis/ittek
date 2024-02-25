2b800fdfed89d864311abe43e4252378394c82d1
Mikkel Jørgensen <mikjorgensen@proton.me>
2024-02-25 15:27:35 +0100

    update gitignore
    added __pycache__ to .gitignore

--- flask/flask_app/app.py
+++ flask/flask_app/app.py
@@ -1,7 +1,23 @@
 # IMPORTS
 from flask import Flask
+from markupsafe import escape
 
 # FLASK
-app = Flask(__name__)
+app = Flask(__name__) # create flask app instance
 
-@
+@app.route('/') # decorator
+@app.route('/index/') # secondary route
+def hello():
+    return '<h1>Hello World!</h1>'
+
+@app.route('/about/')
+def about():
+    return '<h3>This is a Flask web application.</h3>'
+
+@app.route('/capitalize/<word>') # takes text and pass it to view function
+def capitalize(word):
+    return '<h1>{}</h1>'.format(escape(word.capitalize()))
+
+@app.route('/add/<int:n1>/<int:n2>/')
+def add(n1, n2):
+    return ''
