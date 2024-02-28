7cc0e55f0cec1e83f5fb4e31a202fb3fbd71de57
Mikkel Jørgensen <mikjorgensen@proton.me>
2024-02-28 13:18:49 +0100

    add bg-img

--- mqtt/app.py
+++ mqtt/app.py
@@ -1,5 +1,8 @@
 # IMPORTS
+import base64
+from io import BytesIO
 from flask import Flask, render_template
+from matplotlib.figure import Figure
 
 # FLASK
 ## Flask app
@@ -9,6 +12,39 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/kontor')
+def kontor():
+    kontor_temperature = kontor_temp()
+    kontor_humidity = kontor_hum()
+    print(kontor_temperature)
+    print(kontor_humidity)
+    return render_template('kontor.html', kontor_temperature = kontor_temperature, kontor_humidity = kontor_humidity) 
+
+# FUNCTIONS
+def kontor_temp():
+    # Generate the figure **without using pyplot**.
+    fig = Figure()
+    ax = fig.subplots()
+    ax.plot([1, 2])
+    # Save it to a temporary buffer.
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    # Embed the result in the html output.
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return data
+
+def kontor_hum():
+      # Generate the figure **without using pyplot**.
+    fig = Figure()
+    ax = fig.subplots()
+    ax.plot([2, 1])
+    # Save it to a temporary buffer.
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    # Embed the result in the html output.
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return data
 
 # SCRIPT
 app.run(debug=True) # runs with debug
