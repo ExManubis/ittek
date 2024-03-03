aface89384ad6729ed11d67441985a853c111fb9
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2024-03-03 20:11:23 +0100

    mqtt

--- IOT2/log_dht11_data.py
+++ IOT2/log_dht11_data.py
@@ -1,15 +1,37 @@
 # IMPORTS
 import sqlite3
+import paho.mqtt.subscribe as subscribe
 from random import randint
 from datetime import datetime
 from time import sleep
 
-# VARIABLES AND OBJECTS
+print('Subscribe MQTT script running...')
 
-#query = """INSERT INTO stue (datetime, temperature, humidity) VALUES(?, ?, ?)"""
-#data = (datetime.now(), randint(0, 30), randint(0, 100))
+
+
+subscribe.callback(on_message_print, "paho/test/topic", hostname="20.13.128.184", userdata={"message_count": 0})
 
 # FUNCTIONS
+def on_message_print(client, userdata, message):
+    print("%s %s" % (message.topic, message.payload))
+    query = """INSERT INTO stue (datetime, temperature, humidity) VALUES(?, ?, ?)"""
+    now = datetime.now()
+    now = now.strftime('%d/%m/%y %H:%M:%S')
+    data = (now, randint(0, 30), randint(0, 100))
+    
+    try:
+        conn = sqlite3.connect('database/sensor_data.db')
+        cur = conn.cursor()
+        cur.execute(query, data)
+        conn.commit()
+    except sqlite3.Error as sqlite_e:
+        print(f'SQLite error occured')
+        conn.rollback()
+    except Exception as e:
+        print(f'Error occured')
+    finally:
+        conn.close()
+
 def create_table():
     query = """CREATE TABLE IF NOT EXISTS stue(ID INTEGER PRIMARY KEY AUTOINCREMENT, datetime TEXT NOT NULL, temperature REAL NOT NULL, humidity REAL NOT NULL);"""
     try:
@@ -24,26 +46,6 @@ def create_table():
     finally:
         conn.close()
     sleep(1)
-
-def log_stue_dht11():
-    while True:
-        query = """INSERT INTO stue (datetime, temperature, humidity) VALUES(?, ?, ?)"""
-        now = datetime.now()
-        now = now.strftime('%d/%m/%y %H:%M:%S')
-        data = (now, randint(0, 30), randint(0, 100))
-        try:
-            conn = sqlite3.connect('database/sensor_data.db')
-            cur = conn.cursor()
-            cur.execute(query, data)
-            conn.commit()
-        except sqlite3.Error as sqlite_e:
-            print(f'SQLite error occured')
-            conn.rollback()
-        except Exception as e:
-            print(f'Error occured')
-        finally:
-            conn.close()
-        sleep(1)
 
 # SCRIPT
 while True:
--- IOT2/rpie/publish_test.py
+++ IOT2/rpie/publish_test.py
@@ -1,3 +1,6 @@
 import paho.mqtt.publish as publish
+import json
 
-publish.single("paho/test/topic", "payload", hostname="20.13.128.184")
+while True:
+    payload = input('Input message: \n')
+    publish.single("paho/test/topic", payload, hostname="20.13.128.184")
