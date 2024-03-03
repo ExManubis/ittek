bec02884dc331892539e380af9f476b4fb00b5d4
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2024-03-03 13:03:41 +0100

    commit

--- IOT2/app.py
+++ IOT2/app.py
@@ -1,8 +1,36 @@
 # IMPORTS
+import base64
+from io import BytesIO
+from matplotlib.figure import Figure
 from flask import Flask, render_template
 
 # FLASK APP
 app = Flask(__name__)
+
+# FUNCTIONS
+def stue_temp():
+    # Generate the figure **without using pyplot**.
+    fig = Figure()
+    ax = fig.subplots()
+    ax.plot([1, 2])
+    # Save it to a temporary buffer.
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    # Embed the result in the html output.
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return data
+
+def stue_hum():
+    # Generate the figure **without using pyplot**.
+    fig = Figure()
+    ax = fig.subplots()
+    ax.plot([2, 1])
+    # Save it to a temporary buffer.
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    # Embed the result in the html output.
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return data
 
 # FLASK ROUTES
 ## Home
@@ -12,7 +40,9 @@ def home():
 
 @app.route('/stue')
 def stue():
-    return render_template('stue.html')
+    stue_temperature = stue_temp()
+    stue_humidity = stue_hum()
+    return render_template('stue.html', stue_temperature = stue_temperature, stue_humidity = stue_humidity)
 
 # SCRIPT
 app.run(debug=True)
